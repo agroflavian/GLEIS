efa5620fc229d8267c22d5df8e43c1024f8ad300
agroflavian <ftschurr@student.ethz.ch>
2018-11-30 12:03:19 +0100

    adding existing file

---
+++ R/cedi.R
@@ -0,0 +1,17 @@
+#' A classical-CEDI Function
+#'
+#' This function allows to test my package.
+#' @param try  Defaults to TRUE.
+#' @return string of characters
+#' @keywords test
+#' @export
+#' @examples c <- test_function(TRUE), print(c) / c <-test_function(FALSE), print(c)
+#' test_function()
+cedi <- function(try=TRUE){
+  if(try==TRUE){
+    print("eh ciao, alles klar? hesch gwüsst.. FALSE gebet nur chranki Sieche ide funktion ii..?")
+  }
+  else {
+    print("Du bisch doch en chranke Siech alte! Efach FALSE schriibe, weisch was FALSE isch?")
+  }
+}
