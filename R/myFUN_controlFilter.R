e044e2d66d865bbeed1031194459a9299940346a
agroflavian <ftschurr@student.ethz.ch>
2018-11-30 11:18:06 +0100

    Add existing file

---
+++ R/myFUN_controlFilter.R
@@ -0,0 +1,18 @@
+#' filter function --> GLEIS
+#'
+#' filtering data to control treatment relevant
+#' @param Entry nummer of Population (in myGWC )
+#' @return returns a dataframe with the filtered values
+#' @keywords filter
+#' @export
+#' @examples
+#'
+#' myFUN_controlFilter()
+myFUN_controlFilter <- function(Entry){
+filterControl <- myGWC[[Entry]]%>%
+  filter(treatment== "control")%>%
+  filter(experiment %in% myMaxLcum[[Entry]])%>%
+  filter(daytime== TRUE)%>%
+  filter(gwc_dry > 0.5)
+return(data.frame(filterControl))
+}
